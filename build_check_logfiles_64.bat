25074d3a96a375c18239269ae015e5b9846e1810
UrBnW <40244829+UrBnW@users.noreply.github.com>
2020-09-03 00:01:35 +0200

    Build check_logfiles

---
+++ build_check_logfiles_64.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+SET PERL_INSTALL_DIR=C:\Strawberry
+SET PAR_VERBATIM=1
+
+CD check_logfiles
+CMD /C %PERL_INSTALL_DIR%\perl\bin\perl winconfig.pl --with-perl=%PERL_INSTALL_DIR%\perl\bin\perl
+
+CD plugins-scripts
+CMD /C %PERL_INSTALL_DIR%\perl\site\bin\pp ^
+-o check_logfiles_x64.exe check_logfiles ^
+-M PerlIO ^
+-M Digest::MD5 ^
+-M Encode::Encoding ^
+-M Encode::Unicode ^
+-M Encode::Unicode::UTF7 ^
+-M Net::Domain ^
+-M Win32::NetResource ^
+-M Win32::Daemon ^
+-M Time::Piece ^
+-M Time::Local ^
+-M Win32::EventLog ^
+-M Win32::TieRegistry ^
+-M Win32::WinError ^
+-M Date::Manip ^
+-M Win32::OLE ^
+-M Win32::Service ^
+--verbose
