9c3cf03e3489b65e2826dd8c1fc49aea4b710f50
garnier-quentin <garnier.quentin@gmail.com>
2021-09-23 16:00:11 +0200

    fix(plugins): add Safe module

--- build_centreon_plugins_64.bat
+++ build_centreon_plugins_64.bat
@@ -86,6 +86,7 @@ CMD /C %PERL_INSTALL_DIR%\perl\site\bin\pp --lib=centreon-plugins\ ^
 -M Tie::RefHash::Weak ^
 -M Win32::Job ^
 -M XML::LibXML::SAX ^
+-M Safe ^
 -M apps::backup::netbackup::local::plugin ^
 -M apps::backup::netbackup::local::mode::dedupstatus ^
 -M apps::backup::netbackup::local::mode::drivecleaning ^
